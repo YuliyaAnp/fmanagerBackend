7511d5ba14dde94d62402f771e7b84253e733d42
Yuliya Anpilova <anpilova@yuliyas-mbp.home>
2018-02-20 23:55:46 +0000

    addded transaction controller

---
+++ fmanagerBackend/Models/Transaction.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace fmanagerBackend.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public int Sum { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ fmanagerBackend/Models/TransactionContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace fmanagerBackend.Models
+{
+    public class TransactionContext : DbContext
+    {
+        public TransactionContext(DbContextOptions<TransactionContext> options): base(options)
+        {
+        }
+
+        public DbSet<Transaction> Transaction { get; set; }
+    }
+}
